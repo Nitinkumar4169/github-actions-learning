7d47d0493d3a2ba50826e218f237e7678d9167e9
Nitinkumar4169 <nitinkmr.4169@gmail.com>
2026-07-22 10:34:14 +0000

    Updated Node application testing with Express and Supertest

--- node-app/app.js
+++ node-app/app.js
@@ -10,8 +10,15 @@ app.get("/health", (req, res) => {
     res.send("Application is healthy");
 });
 
-const PORT = 3001;
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+
+    const PORT = 3001;
+
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+
+}
+
+module.exports = app;

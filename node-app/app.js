cfa18ec0b19e577e449d11e815e6ffc8574751b3
Nitinkumar4169 <nitinkmr.4169@gmail.com>
2026-07-22 10:06:36 +0000

    Added self hosted runner docker test workflow

--- node-app/app.js
+++ node-app/app.js
@@ -1,5 +1,17 @@
-function add(a, b) {
-    return a + b;
-}
+const express = require("express");
 
-module.exports = add;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from GitHub Actions Self-Hosted Runner!");
+});
+
+app.get("/health", (req, res) => {
+    res.send("Application is healthy");
+});
+
+const PORT = 3001;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
